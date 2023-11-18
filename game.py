71079e20b6899155ed72fa42ef2320249659ccf5
maureenfioreen <fiorinimauro5@gmail.com>
2023-11-18 19:24:00 +0100

    moved the player functions to its file

--- game.py
+++ game.py
@@ -2,7 +2,7 @@
     TODO:
     - implement prompts
     - draw ladders and snakes 
-    - impplement special events, of which there are two: 
+    - implement special events, of which there are two: 
         + timeout event 
         + wait-one-turn event
     - dice mechanism
@@ -10,21 +10,10 @@
 '''
 import colors
 import board 
-import players
 import pygame
-
-# Everything inherent to the working plane
-CANVAS_X = 960
-CANVAS_Y = 720
-screen = pygame.display.set_mode((CANVAS_X, CANVAS_Y), pygame.RESIZABLE)
-
-# Variables 
-game_in_progress = True 
-
-# Template measurements
-tile_size = screen.get_height() / 10
-border_distance_x : int = (screen.get_width() / 2) - (tile_size * board.B.side_len / 2)
-border_distance_y : int = screen.get_height() / 30
+import mr305
+from players import P1, P2
+from time import sleep 
 
 
 # Functions
@@ -36,7 +25,7 @@ def init_game() -> None:
 
 
 def draw_board() -> None: 
-    screen.fill(colors.BG_COLOR)
+    mr305.screen.fill(colors.BG_COLOR)
 
     # Here we take the list we compiled in board.py and draw the single tiles 
     # with a number in the middle for each element
@@ -49,80 +38,66 @@ def draw_board() -> None:
             if element.index == 1 or element.index == board.B.side_len**2: tile_color = colors.YELLOWISH
             else: tile_color = element.color      
             
-            tile = pygame.Rect(border_distance_x + offset_x, border_distance_y + offset_y, tile_size, tile_size)
+            tile = pygame.Rect(mr305.border_distance_x + offset_x, mr305.border_distance_y + offset_y, mr305.tile_size, mr305.tile_size)
             text_surface_object = pygame.font.SysFont('Arial', 25).render(str(element.index), True, colors.BLACK)
             text_rect = text_surface_object.get_rect(center=tile.center)
-            pygame.draw.rect(screen, tile_color, tile)
-            pygame.draw.rect(screen, colors.WHITE, tile, 2, border_radius=1)
-            screen.blit(text_surface_object, text_rect)
-            offset_x += tile_size
+            pygame.draw.rect(mr305.screen, tile_color, tile)
+            pygame.draw.rect(mr305.screen, colors.WHITE, tile, 2, border_radius=1)
+            mr305.screen.blit(text_surface_object, text_rect)
+            offset_x += mr305.tile_size
 
         offset_x = 0
-        offset_y += tile_size
+        offset_y += mr305.tile_size
     
-    msg_prompt = pygame.Rect(border_distance_x - tile_size, border_distance_y + offset_y, tile_size * (board.B.side_len + 2), tile_size * 1.5)
-    pygame.draw.rect(screen, colors.BG_COLOR, msg_prompt)
-    pygame.draw.rect(screen, colors.WHITE, msg_prompt, 2, border_radius=1)
+    msg_prompt = pygame.Rect(mr305.border_distance_x - mr305.tile_size, mr305.border_distance_y + offset_y, mr305.tile_size * (board.B.side_len + 2), mr305.tile_size * 1.5)
+    pygame.draw.rect(mr305.screen, colors.BG_COLOR, msg_prompt)
+    pygame.draw.rect(mr305.screen, colors.WHITE, msg_prompt, 2, border_radius=1)
 
 
-# Player functions 
-def from_index_to_coords(index : int) -> tuple: 
-    # Okay so here we calculate the different coordinates based on the index
-    offset_from_goal = tuple()
-    x : int = 0
-    y : int = 0
+def move_player(player, new_position): 
+    if player.position < new_position: 
+        player.position += 1
 
-    starting_coordinates = (border_distance_x + tile_size/2, border_distance_y + tile_size/2)
+    elif player.position > new_position: 
+        player.position -= 1
     
-    # In [0] we save how many times the number can be divided by the length of the board, in 
-    # [1] we save how much remains
-    offset_from_goal = divmod(index - 1, board.B.side_len)
+    player.draw_player()
+    draw_board()
+    sleep(0.3)
 
-    # y-coordinate  
-    y = starting_coordinates[1] + ((board.B.side_len - offset_from_goal[0] - 1) * tile_size)    
+def render():  
+    # Here the program iterates through a list of various events in order to react to them
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: 
+            mr305.game_in_progress = False 
+        
+        if event.type == pygame.VIDEORESIZE:
+            old_surface_saved = mr305.screen
+            mr305.screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
+            # On the next line, if only part of the window
+            # needs to be copied, there's some other options.
+            mr305.screen.blit(old_surface_saved, (0,0))
+            del old_surface_saved
 
-    # x_coordinate
-    if offset_from_goal[0] % 2 != 0: 
-        x = starting_coordinates[0] + ((offset_from_goal[1]) * tile_size)
-    else: 
-        x= starting_coordinates[0] + ((board.B.side_len - offset_from_goal[1] - 1) * tile_size)
+        # Events with keys
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                mr305.game_in_progress = False
 
-
-    return (x, y)
-    
-
-def draw_player(player) -> pygame.Rect:
-    coords = from_index_to_coords(player.position)
-    pygame.draw.circle(screen, player.color, (coords[0], coords[1] + player.y_offset), player.radius)
+    # Render the board 
+    draw_board()
+    P1.draw_player()
+    P2.draw_player()
+    pygame.display.update()
 
 
 # Drawing the game 
 if __name__ == '__main__':
     init_game()
-
-    while game_in_progress: 
-        # Here the program iterates through a list of various events in order to react to them
-        for event in pygame.event.get(): 
-            if event.type == pygame.QUIT: 
-                game_in_progress = False 
-            
-            if event.type == pygame.VIDEORESIZE:
-                old_surface_saved = screen
-                screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
-                # On the next line, if only part of the window
-                # needs to be copied, there's some other options.
-                screen.blit(old_surface_saved, (0,0))
-                del old_surface_saved
-
-            # Events with keys
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    game_in_progress = False
-
-        # Render the board 
-        draw_board()
-        draw_player(players.P1)
-        draw_player(players.P2)
-        pygame.display.update()
-
+    while mr305.game_in_progress: 
+        render()
+        move_player(P1, 24)
+        move_player(P2, 15)
+    
+        
     pygame.quit()
--- players.py
+++ players.py
@@ -1,4 +1,9 @@
 import colors
+import mr305
+import board
+
+from pygame import draw, display
+
 
 class Player():
     def __init__(self, id : str, color : tuple,  y_offset : int, position : int = 1):
@@ -9,11 +14,40 @@ class Player():
         self.color = color
         self.radius = 10
 
-        # This is fo the animation
-        self.moving_direction = -1
     
     def __repr__(self):
         return self.id
     
+    # Player functions 
+    def from_index_to_coords(self) -> tuple: 
+        # Okay so here we calculate the different coordinates based on the index
+        offset_from_goal = tuple()
+        x : int = 0
+        y : int = 0
+
+        starting_coordinates = (mr305.border_distance_x + mr305.tile_size/2, mr305.border_distance_y + mr305.tile_size/2)
+        
+        # In [0] we save how many times the number can be divided by the length of the board, in 
+        # [1] we save how much remains
+        offset_from_goal = divmod(self.position - 1, board.B.side_len)
+
+        # y-coordinate  
+        y = starting_coordinates[1] + ((board.B.side_len - offset_from_goal[0] - 1) * mr305.tile_size)    
+
+        # x_coordinate
+        if offset_from_goal[0] % 2 != 0: 
+            x = starting_coordinates[0] + ((offset_from_goal[1]) * mr305.tile_size)
+        else: 
+            x= starting_coordinates[0] + ((board.B.side_len - offset_from_goal[1] - 1) * mr305.tile_size)
+
+
+        return (x, y)
+        
+
+    def draw_player(self):
+        coords = self.from_index_to_coords()
+        draw.circle(mr305.screen, self.color, (coords[0], coords[1] + self.y_offset), self.radius)
+       
+
 P1 = Player('Position_22', colors.PLAYER_1_COL, -20)  
 P2 = Player('Position_14', colors.PLAYER_2_COL, 20)  
