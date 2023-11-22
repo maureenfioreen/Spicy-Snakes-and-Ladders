d5bc702a331dc0bb51e172ec61f321c1dd79929c
maureenfioreen <fiorinimauro5@gmail.com>
2023-11-23 00:03:16 +0100

    minor changes

--- dice.py
+++ dice.py
@@ -1,11 +1,10 @@
 from os import path
 from pygame.image import load
 from pygame.transform import scale
-from pygame.display import update
 from mr305 import screen
 
 
-def render_dice(index : int = 0):
+def render_dice(player_dice_coords : tuple, index : int = 0):
     dice_image = load(path.join('./Dice_Faces', '{}.png'.format(index)))
-    screen.blit(scale(dice_image, (100, 100)), (10, 10))
-    update()
+    screen.blit(scale(dice_image, (100, 100)), player_dice_coords)
+     
--- mr305.py
+++ mr305.py
@@ -1,5 +1,9 @@
-from pygame import display, RESIZABLE
+from pygame import display, Rect, RESIZABLE
 from board import B
+
+# haha get it since it's full of globally used stuff
+# global kinda means woldwide like pitbull, mr.305
+# dale papichulo
 
 # Everything inherent to the working plane
 CANVAS_X = 960
@@ -8,8 +12,13 @@ CANVAS_Y = 720
 # Variables 
 game_in_progress = True 
 screen = display.set_mode((CANVAS_X, CANVAS_Y), RESIZABLE)
+global msg_prompt 
 
 # Template measurements
 tile_size = screen.get_height() / 10
 border_distance_x : int = (screen.get_width() / 2) - (tile_size * B.side_len / 2)
 border_distance_y : int = screen.get_height() / 30
+
+# Coordinates to render the dice 
+dice_player_1_coords =  (50, 10)
+dice_player_2_coords = (CANVAS_X - 150, 10)
