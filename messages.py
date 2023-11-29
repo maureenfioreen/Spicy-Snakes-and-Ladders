0a491cca2a9334d7d0b3692d04da2e48e6b89759
maureenfioreen <fiorinimauro5@gmail.com>
2023-11-29 19:05:01 +0100

    removed the mutex

--- messages.py
+++ messages.py
@@ -1,1 +1,27 @@
 # Here is everything relating to messages 
+from pygame import draw, Rect, font 
+from pygame.display import update
+from colors import WHITE, BG_COLOR
+from mr305 import border_distance_x, border_distance_y, tile_size, screen, side_length
+from time import sleep
+
+
+def draw_message_prompt(): 
+    msg_prompt = Rect(border_distance_x - tile_size, border_distance_y + side_length * tile_size,  tile_size * (side_length + 2), tile_size * 1.5)
+    draw.rect(screen, BG_COLOR, msg_prompt)
+    draw.rect(screen, WHITE, msg_prompt, 2, border_radius=1) 
+
+
+def print_message(message): 
+    #with coord_mutex:
+    text_font =  font.Font(None, 24)
+    text = text_font.render(message, True, WHITE)
+    text_rect = text.get_rect()
+    text_rect.topleft = (border_distance_x - tile_size + 10, border_distance_y + 10 + side_length * tile_size)
+    screen.blit(text, text_rect)
+
+        
+    
+    
+        
+
