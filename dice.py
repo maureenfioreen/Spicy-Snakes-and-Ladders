eb0fe42de328a1147fca0e429e5cb5fec544d54c
maureenfioreen <fiorinimauro5@gmail.com>
2023-11-29 19:07:08 +0100

    tried to change stuff, did not succeed

--- dice.py
+++ dice.py
@@ -7,4 +7,5 @@ from mr305 import screen
 def render_dice(player_dice_coords : tuple, index : int = 0):
     dice_image = load(path.join('./Dice_Faces', '{}.png'.format(index)))
     screen.blit(scale(dice_image, (100, 100)), player_dice_coords)
+    
      
